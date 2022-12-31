7554fb15cb50f5f5c524f86a9e4d4d8421ac1f94
ayushkr1701 <ayushkr1701@gmail.com>
2022-12-31 22:16:23 +0530

    first

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableWithoutFeedback, TouchableOpacity, Image, SafeAreaView, TouchableHighlight, TouchableNativeFeedback, Alert } from 'react-native';
+import { Button } from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
 
 export default function App() {
+  // console.log(useDimensions());
+  // const handleProcess = () => console.log("Text Pressed")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text onPress={handleProcess}>It's me EdiTh</Text>
+      <TouchableNativeFeedback onPress={()=> console.log("tapped image")}>
+
+        <Image source={{
+        width:200,
+        height: 300,
+        uri: "https://res.cloudinary.com/dtjivws2c/image/upload/v1/media/images/PFP50x70_bwwfyx" } }/>
+      </TouchableNativeFeedback>
+      <Text>I'm a web/app developer</Text> */}
+      {/* <Button>Hello</Button> */}
+      {/* <Button
+      color="orange" 
+      title="Click Me" 
+      onPress={()=>Alert.prompt("My title", "My message", text => console.log(text))}/>
+      <StatusBar style="auto" /> */}
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%",
+          alignItems:"center",
+          justifyContent:"center"
+        }}>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  Title:{
+    fontSize:50,
+    color:'black',
+    fontWeight:'bold',
+    textAlign: 'center'
+},
+subTitle:{
+  fontSize:30,
+  color:'black',
+  fontWeight:'normal',
+  textAlign: 'center'
+},
+
 });

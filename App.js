cf7d99fd4be6672dd9899830b1a8dc7ccf37e4af
ayushkr1701 <ayushkr1701@gmail.com>
2023-01-04 02:42:24 +0530

    made first page of app

--- App.js
+++ App.js
@@ -1,57 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, TouchableWithoutFeedback, TouchableOpacity, Image, SafeAreaView, TouchableHighlight, TouchableNativeFeedback, Alert } from 'react-native';
 import { Button } from 'react-native';
-import { useDimensions } from '@react-native-community/hooks';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  // console.log(useDimensions());
+const{landscape}=(useDeviceOrientation());
   // const handleProcess = () => console.log("Text Pressed")
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <Text onPress={handleProcess}>It's me EdiTh</Text>
-      <TouchableNativeFeedback onPress={()=> console.log("tapped image")}>
+    <WelcomeScreen/>
 
-        <Image source={{
-        width:200,
-        height: 300,
-        uri: "https://res.cloudinary.com/dtjivws2c/image/upload/v1/media/images/PFP50x70_bwwfyx" } }/>
-      </TouchableNativeFeedback>
-      <Text>I'm a web/app developer</Text> */}
-      {/* <Button>Hello</Button> */}
-      {/* <Button
-      color="orange" 
-      title="Click Me" 
-      onPress={()=>Alert.prompt("My title", "My message", text => console.log(text))}/>
-      <StatusBar style="auto" /> */}
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: "30%",
-          alignItems:"center",
-          justifyContent:"center"
-        }}>
-      </View>
-    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-  },
-  Title:{
-    fontSize:50,
-    color:'black',
-    fontWeight:'bold',
-    textAlign: 'center'
-},
-subTitle:{
-  fontSize:30,
-  color:'black',
-  fontWeight:'normal',
-  textAlign: 'center'
-},
 
-});
